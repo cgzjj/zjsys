d44f4dfe980cee5f78a5446e4f9c684226c8dc53
yuanyuecan1995 <bclz9518@126.com>
2020-11-19 14:00:14 +0800

    第一次上传

---
+++ src/main/java/com/hyxc/zjsys/ZjsysApplication.java
@@ -0,0 +1,13 @@
+package com.hyxc.zjsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyxc/zjsys/ZjsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyxc.zjsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
